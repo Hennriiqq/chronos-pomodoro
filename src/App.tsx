17b369b6134b330c899ab3a023488b75285c9956
Carlos Henrique <leitecarlos53@gmail.com>
2026-09-10 09:29:47 -0300

    Criando a estrutura HTML e CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá Mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos odit
-        consequuntur repellat assumenda dolorum soluta magni cumque voluptatum!
-        Veritatis quidem accusamus ea rerum aspernatur dignissimos quasi
-        explicabo, natus reiciendis iure?
-      </p>
+      <div className='container'>
+        <div className='content'>
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className='container'>
+        <div className='content'>
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
